bea063c228c6c7b205613f991217ef38dfe9dad4
martimalexander <120787261+martimalexander@users.noreply.github.com>
2024-02-08 14:38:03 +0530

    dheh

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import { Hono, ResponseBuilder } from 'hono';
 
 // Create a new Hono instance
-const hono = new Hono();
+const app = new Hono();
 
 // Define route for root endpoint
-hono.get('/', (_, response) => {
-  return response
-    .status(200)
-    .contentType('text/plain')
-    .send('CF Hono Working');
-});
+app.get('/', (c) => {
+  return c.text("Hey server is running");
+})
 
 // Export the Hono instance as the default app
 export default hono;
